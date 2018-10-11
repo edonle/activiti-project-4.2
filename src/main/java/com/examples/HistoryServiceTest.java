2ed349e509eed47e89f350c038c6c6d2b68319a0
Edson-Prodesp <edsonsilva@sp.gov.br>
2018-10-11 11:52:56 -0300

    Retrieving process variable updates with the HistoryService interface.

--- src/main/java/com/examples/HistoryServiceTest.java
+++ src/main/java/com/examples/HistoryServiceTest.java
@@ -10,7 +10,9 @@ import org.activiti.engine.HistoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricActivityInstance;
+import org.activiti.engine.history.HistoricDetail;
 import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.history.HistoricVariableUpdate;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
@@ -25,12 +27,16 @@ public class HistoryServiceTest extends AbstractTest {
 		RuntimeService runtimeService = activitiRule.getRuntimeService();
 		Map<String, Object> variableMap = new HashMap<String, Object>();
 		variableMap.put("isbn", "123456");
+
+		// Starts a new process instance
 		String processInstanceID = runtimeService.startProcessInstanceByKey("bookorder", variableMap).getId();
 		TaskService taskService = activitiRule.getTaskService();
 		Task task = taskService.createTaskQuery().taskCandidateGroup("sales").singleResult();
 		variableMap = new HashMap<String, Object>();
 		variableMap.put("extraInfo", "Extra information");
 		variableMap.put("isbn", "654321");
+
+		// Completes a user task with variables
 		taskService.complete(task.getId(), variableMap);
 		return processInstanceID;
 	}
@@ -40,6 +46,8 @@ public class HistoryServiceTest extends AbstractTest {
 	public void queryHistoricInstances() {
 		String processInstanceID = startAndComplete();
 		HistoryService historyService = activitiRule.getHistoryService();
+
+		// Queries for historic process instances
 		HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
 				.processInstanceId(processInstanceID).singleResult();
 		assertNotNull(historicProcessInstance);
@@ -55,6 +63,8 @@ public class HistoryServiceTest extends AbstractTest {
 	public void queryHistoricActivities() {
 		startAndComplete();
 		HistoryService historyService = activitiRule.getHistoryService();
+
+		// Queries for historic activities
 		List<HistoricActivityInstance> activityList = historyService.createHistoricActivityInstanceQuery().list();
 		assertEquals(3, activityList.size());
 		for (HistoricActivityInstance historicActivityInstance : activityList) {
@@ -64,4 +74,31 @@ public class HistoryServiceTest extends AbstractTest {
 					+ historicActivityInstance.getDurationInMillis());
 		}
 	}
+
+	@Test
+	@Deployment(resources = { "bookorder.bpmn20.xml" })
+	public void queryHistoricVariableUpdates() {
+		startAndComplete();
+		HistoryService historyService = activitiRule.getHistoryService();
+		
+		//Queries process variable updates
+		List<HistoricDetail> historicVariableUpdateList = historyService.createHistoricDetailQuery().variableUpdates()
+				.list();
+		assertNotNull(historicVariableUpdateList);
+		assertEquals(3, historicVariableUpdateList.size());
+		for (HistoricDetail historicDetail : historicVariableUpdateList) {
+			
+			//HistoricVariable Update for process variable updates
+			assertTrue(historicDetail instanceof HistoricVariableUpdate);
+			HistoricVariableUpdate historicVariableUpdate = (HistoricVariableUpdate) historicDetail;
+			assertNotNull(historicVariableUpdate.getExecutionId());
+			
+			//Gets new process variable value
+			System.out.println("historic variable update, revision " + historicVariableUpdate.getRevision()
+					+ ", variable type name " + historicVariableUpdate.getVariableTypeName() + ", variable name "
+					+ historicVariableUpdate.getVariableName() + ", Variable value '"
+					+ historicVariableUpdate.getValue() + "'");
+		}
+	}
+
 }
